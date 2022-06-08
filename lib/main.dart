f12517b66e23db6690b5306982b4f574843305b1
Kako <kakomo2@gmail.com>
2022-06-08 10:46:39 -0300

    Aula 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/screens/form_screen.dart';
 import 'package:nosso_primeiro_projeto/screens/initial_screen.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nova Tarefa'),
+      ),
+      body: Center(
+        child: Container(
+          height: 650,
+          width: 375,
+          decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 3),
+          ),
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
